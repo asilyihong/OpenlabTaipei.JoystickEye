6dc36be592b3d7f55981fb295aa65f6df564e526
asil <asil.yihong@gmail.com>
2015-10-22 23:35:24 +0800

    First release.
    Move eye according to joystick signal.

---
+++ JoystickEye/JoystickEye.ino
@@ -0,0 +1,280 @@
+/* We always have to include the LedControl library */
+#include <LedControl.h>
+#include <Serial.h>
+/*
+   Create LetControl object, define pin connections
+   We have 2 MAX72XX for eyes.
+ */
+#define PIN_EYES_DIN 12
+#define PIN_EYES_CS  10
+#define PIN_EYES_CLK 11
+
+/* Joystick pin */
+#define X_PIN   A1
+#define Y_PIN   A0
+#define BTN_PIN  2
+
+/* define eyeball count */
+#define EYEBALL_CNT  1
+LedControl lc = LedControl(PIN_EYES_DIN, PIN_EYES_CLK, PIN_EYES_CS, EYEBALL_CNT);
+/* rotation */
+bool rotateMatrix0 = false; /* rotate 0 matrix by 180 deg */
+bool rotateMatrix1 = false; /* rotate 1 matrix by 180 deg */
+/* define eye ball without pupil */
+byte eyeBall[8]={
+    B00111100,
+    B01111110,
+    B11111111,
+    B11111111,
+    B11111111,
+    B11111111,
+    B01111110,
+    B00111100
+};
+const byte eyePupil = B00011000;
+/* stores current state of LEDs */
+byte eyeCurrent[8];
+int currentX;
+int currentY;
+int cntLoop = 0;
+int cntEffect = 0;
+#define DELAY_BLINK 40
+/* min and max positions */
+#define MIN -2
+#define MAX 2
+int xPosition = 0;
+int yPosition = 0;
+int buttonState = 0;
+
+/*
+   Arduino setup
+ */
+void setup()
+{
+    // MAX72XX is in power-saving mode on startup, we have to do a wakeup call
+    lc.shutdown(0,false);
+    // set the brightness to low
+    lc.setIntensity(0,1);
+    // clear both modules
+    lc.clearDisplay(0);
+#if (EYEBALL_CNT == 2)
+    lc.shutdown(1,false);
+    lc.setIntensity(1,1);
+    lc.clearDisplay(1);
+#endif
+    // LED test
+    // vertical line
+    byte b = B10000000;
+    for (int c=0; c<=7; c++)
+    {
+        for (int r=0; r<=7; r++)
+        {
+            setRow(0, r, b);
+#if (EYEBALL_CNT == 2)
+            setRow(1, r, b);
+#endif
+        }
+        b = b >> 1;
+        delay(50);
+    }
+    /* full module */
+    b = B11111111;
+    for (int r=0; r<=7; r++)
+    {
+        setRow(0, r, b);
+#if (EYEBALL_CNT == 2)
+        setRow(1, r, b);
+#endif
+    }
+    delay(500);
+    /* clear both modules */
+    lc.clearDisplay(0);
+#if (EYEBALL_CNT == 2)
+    lc.clearDisplay(1);
+#endif
+    delay(500);
+    /* random seed */
+    randomSeed(analogRead(0));
+    /* center eyes, crazy blink */
+    displayEyes(0, 0);
+
+    /* initial Joystick */
+    pinMode(X_PIN, INPUT);
+    pinMode(Y_PIN, INPUT);
+
+    /* activate pull-up resistor on the push-button pin */
+    pinMode(BTN_PIN, INPUT_PULLUP); 
+    Serial.begin(9600);
+
+    delay(1000);
+}
+
+/*
+   Arduino loop
+ */
+void loop()
+{
+    xPosition = map(analogRead(X_PIN), 0, 1024, MIN, MAX + 1);
+    yPosition = map(analogRead(Y_PIN), 0, 1024, MIN, MAX + 1);
+    buttonState = digitalRead(BTN_PIN);
+
+    if (!buttonState)
+    {
+        blinkEyes(true, true);
+    } 
+    else
+    {
+        displayEyes(xPosition, yPosition);
+//        Serial.print("X: ");
+//        Serial.print(xPosition);
+//        Serial.print(" | Y: ");
+//        Serial.println(yPosition);
+    }
+    delay (100);
+}
+/*
+   This method corrects provided coordinate value
+ */
+int getValidValue(int value)
+{
+    if (value > MAX)
+        return MAX;
+    else if (value < MIN)
+        return MIN;
+    else
+        return value;
+}
+/*
+   This method displays eyeball with pupil offset by X, Y values from center position.
+   Valid X and Y range is [MIN,MAX]
+   Both LED modules will show identical eyes
+ */
+void displayEyes(int offsetX, int offsetY)
+{
+    // ensure offsets are in valid ranges
+    offsetX = getValidValue(offsetX);
+    offsetY = getValidValue(offsetY);
+    // calculate indexes for pupil rows (perform offset Y)
+    int row1 = 3 - offsetY;
+    int row2 = 4 - offsetY;
+    // define pupil row
+    byte pupilRow = eyePupil;
+    // perform offset X
+    // bit shift and fill in new bit with 1
+    if (offsetX > 0) {
+        pupilRow = pupilRow >> offsetX;
+    }
+    else if (offsetX < 0) {
+        pupilRow = pupilRow << (-offsetX);
+    }
+    pupilRow = pupilRow ^ B11111111;
+    // pupil row cannot have 1s where eyeBall has 0s
+    byte pupilRow1 = pupilRow & eyeBall[row1];
+    byte pupilRow2 = pupilRow & eyeBall[row2];
+    // display on LCD matrix, update to eyeCurrent
+    for(int r=0; r<8; r++)
+    {
+        if (r == row1)
+        {
+            setRow(0, r, pupilRow1);
+            setRow(1, r, pupilRow1);
+            eyeCurrent[r] = pupilRow1;
+        }
+        else if (r == row2)
+        {
+            setRow(0, r, pupilRow2);
+            setRow(1, r, pupilRow2);
+            eyeCurrent[r] = pupilRow2;
+        }
+        else
+        {
+            setRow(0, r, eyeBall[r]);
+            setRow(1, r, eyeBall[r]);
+            eyeCurrent[r] = eyeBall[r];
+        }
+    }
+    // update current X and Y
+    currentX = offsetX;
+    currentY = offsetY;
+}
+/*
+   This method blinks eyes as per provided params
+ */
+void blinkEyes(boolean blinkLeft, boolean blinkRight)
+{
+    // blink?
+    if (!blinkLeft && !blinkRight)
+        return;
+    // close eyelids
+    for (int i=0; i<=3; i++)
+    {
+        if (blinkLeft)
+        {
+            setRow(0, i, 0);
+            setRow(0, 7-i, 0);
+        }
+        if (blinkRight)
+        {
+            setRow(1, i, 0);
+            setRow(1, 7-i, 0);
+        }
+        delay(DELAY_BLINK);
+    }
+    delay(DELAY_BLINK);
+    // open eyelids
+    for (int i=3; i>=0; i--)
+    {
+        if (blinkLeft)
+        {
+            setRow(0, i, eyeCurrent[i]);
+            setRow(0, 7-i, eyeCurrent[7-i]);
+        }
+        if (blinkRight)
+        {
+            setRow(1, i, eyeCurrent[i]);
+            setRow(1, 7-i, eyeCurrent[7-i]);
+        }
+        delay(DELAY_BLINK);
+    }
+}
+/*
+   This method sets values to matrix row
+   Performs 180 rotation if needed
+ */
+void setRow(int addr, int row, byte rowValue)
+{
+    if (((addr == 0) && (rotateMatrix0)) || (addr == 1 && rotateMatrix1))
+    {
+        row = abs(row - 7);
+        rowValue = bitswap(rowValue);
+    }
+    lc.setRow(addr, row, rowValue);
+}
+/*
+   Reverse bits in byte
+http://www.nrtm.org/index.php/2013/07/25/reverse-bits-in-a-byte/
+ */
+byte bitswap (byte x)
+{
+    byte result;
+    asm("mov __tmp_reg__, %[in] \n\t"
+            "lsl __tmp_reg__ \n\t" /* shift out high bit to carry */
+            "ror %[out] \n\t" /* rotate carry __tmp_reg__to low bit (eventually) */
+            "lsl __tmp_reg__ \n\t" /* 2 */
+            "ror %[out] \n\t"
+            "lsl __tmp_reg__ \n\t" /* 3 */
+            "ror %[out] \n\t"
+            "lsl __tmp_reg__ \n\t" /* 4 */
+            "ror %[out] \n\t"
+            "lsl __tmp_reg__ \n\t" /* 5 */
+            "ror %[out] \n\t"
+            "lsl __tmp_reg__ \n\t" /* 6 */
+            "ror %[out] \n\t"
+            "lsl __tmp_reg__ \n\t" /* 7 */
+            "ror %[out] \n\t"
+            "lsl __tmp_reg__ \n\t" /* 8 */
+            "ror %[out] \n\t"
+            : [out] "=r" (result) : [in] "r" (x));
+    return(result);
+}
+
